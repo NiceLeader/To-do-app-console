2772980120a280077b93a9ca2a288fedba260d5a
marek wasiewski <108329227+marwas97@users.noreply.github.com>
2022-09-04 19:41:35 +0200

    v1.2

--- src/main/java/testarmy/JDBC.java
+++ src/main/java/testarmy/JDBC.java
@@ -3,18 +3,20 @@ import org.mindrot.jbcrypt.BCrypt;
 
 import java.sql.*;
 import java.util.Scanner;
+
+import static java.lang.System.exit;
+
 public class JDBC {
     static final String DATABASE_URL = "jdbc:mysql://sql11.freesqldatabase.com/sql11517150";
-    protected static  String DATABASE_USERNAME = "sql11517150";
+    protected static String DATABASE_USERNAME = "sql11517150";
     protected int returnGeneratedKeys = Statement.RETURN_GENERATED_KEYS;
-    protected static  String DATABASE_PASSWORD = "vG3TwK9hpD";
-    protected static  String SELECT_QUERY = "SELECT * FROM registration WHERE email_id = ? and password = ?";
+    protected static String DATABASE_PASSWORD = "vG3TwK9hpD";
+    protected static String SELECT_QUERY = "SELECT * FROM registration WHERE email_id = ? and password = ?";
     protected String name;
     protected static String salt = BCrypt.gensalt();
 
 
-
-    public boolean validate(String emailId, String password){
+    public boolean validate(String emailId, String password) {
 
         try (Connection connection = DriverManager
                 .getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
@@ -36,6 +38,7 @@ public class JDBC {
         }
         return false;
     }
+
     public static boolean loginUser(Connection connection) throws SQLException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("podaj login");
@@ -49,26 +52,19 @@ public class JDBC {
         pass.setString(1, login);
         pass.execute();
         statement.setString(1, login);
-
-//        if (BCrypt.checkpw(password, sqlPswd)) {
-//            statement.setString(2, password);
-//           String sqlName = "SELECT name FROM user WHERE login=?;";
-//           PreparedStatement name = connection.prepareStatement(sqlName);
-//           name.setString(1,login);
-//            statement.executeUpdate();
-//        }else {
-//        System.out.println("błędne hasło");}
-        boolean i = pass.execute();
-        if (i == true) {
-            System.out.println("Zalogowałeś się!");
-        } else {
-            System.out.println("Nie zalogowałeś się!");
+        if (BCrypt.checkpw(password, sqlPswd)) {
+            statement.setString(2, password);
+            String sqlName = "SELECT name FROM user WHERE login=?;";
+            PreparedStatement name = connection.prepareStatement(sqlName);
+            name.setString(1, login);
+            statement.executeUpdate();
         }
-       return true;
+        System.out.println("błędne hasło");
+        return true;
     }
 
     public static void printSQLException(SQLException ex) {
-        for (Throwable e: ex) {
+        for (Throwable e : ex) {
             if (e instanceof SQLException) {
                 e.printStackTrace(System.err);
                 System.err.println("SQLState: " + ((SQLException) e).getSQLState());
@@ -82,41 +78,42 @@ public class JDBC {
             }
         }
     }
+
     private static void createTableTask(Connection connection) throws SQLException {
         String sqlCreateTableTask = """
-                    CREATE TABLE IF NOT EXISTS task(
-                            id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                            title VARCHAR(255) NOT NULL,
-                            description TEXT NOT NULL,
-                            done BOOLEAN NOT NULL DEFAULT FALSE,
-                            created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                            priority ENUM ('wysoki', 'średni', 'niski') NOT NULL DEFAULT 'niski',
-                            user_id INT NOT NULL,
-                            FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE);
-                                                 """;
+                CREATE TABLE IF NOT EXISTS task(
+                        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                        title VARCHAR(255) NOT NULL,
+                        description TEXT NOT NULL,
+                        done BOOLEAN NOT NULL DEFAULT FALSE,
+                        created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                        priority ENUM ('wysoki', 'średni', 'niski') NOT NULL DEFAULT 'niski',
+                        user_id INT NOT NULL,
+                        FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE);
+                                             """;
         PreparedStatement preparedStatement = connection.prepareStatement(sqlCreateTableTask);
         preparedStatement.executeUpdate();
     }
 
     private static void createTableUser(Connection connection) throws SQLException {
         String sqlCreateTableUser = """
-                    CREATE TABLE IF NOT EXISTS user(
-                            id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                            name VARCHAR(128) NOT NULL,
-                            surname VARCHAR(255) NOT NULL,
-                            login VARCHAR(128) NOT NULL UNIQUE,
-                            password CHAR(60) NOT NULL,
-                            email VARCHAR(255) UNIQUE);
-                                                 """;
+                CREATE TABLE IF NOT EXISTS user(
+                        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                        name VARCHAR(128) NOT NULL,
+                        surname VARCHAR(255) NOT NULL,
+                        login VARCHAR(128) NOT NULL UNIQUE,
+                        password CHAR(60) NOT NULL,
+                        email VARCHAR(255) UNIQUE);
+                                             """;
         PreparedStatement preparedStatement = connection.prepareStatement(sqlCreateTableUser);
         preparedStatement.executeUpdate();
     }
 
     public static void addTask(Connection connection) throws SQLException {
         String sqlAddTask = """
-                     INSERT INTO task(title,description,priority,user_id) VALUES
-                    (?,?,?,?)
-                        """;
+                 INSERT INTO task(title,description,priority,user_id) VALUES
+                (?,?,?,?)
+                    """;
         Scanner scanner = new Scanner(System.in);
         System.out.println("podaj nazwę zadania");
         String title = scanner.nextLine();
@@ -142,14 +139,15 @@ public class JDBC {
         String status = scanner.nextLine();
         String sqlUpdate = "UPDATE task SET done = ? WHERE id = ?";
         PreparedStatement preparedStatement = connection.prepareStatement(sqlUpdate);
-        preparedStatement.setString(1,status);
-        preparedStatement.setInt(2,Integer.parseInt(id));
+        preparedStatement.setString(1, status);
+        preparedStatement.setInt(2, Integer.parseInt(id));
         preparedStatement.executeUpdate();
     }
+
     private static void createDatabase(Connection connection) throws SQLException {
         String sqlCreateDatabase = """
-                    CREATE DATABASE IF NOT EXISTS to_do_app;
-                                        """;
+                CREATE DATABASE IF NOT EXISTS to_do_app;
+                                    """;
         PreparedStatement preparedStatement = connection.prepareStatement(sqlCreateDatabase);
         preparedStatement.execute();
     }
@@ -168,9 +166,9 @@ public class JDBC {
         System.out.println("podaj email");
         String email = scanner.nextLine();
         String sqlAddTask = """
-                     INSERT INTO user(name,surname,login,password,email) VALUES
-                    (?,?,?,?,?)
-                        """;
+                 INSERT INTO user(name,surname,login,password,email) VALUES
+                (?,?,?,?,?)
+                    """;
         PreparedStatement preparedStatement = connection.prepareStatement(sqlAddTask, Statement.RETURN_GENERATED_KEYS);
         preparedStatement.setString(1, name);
         preparedStatement.setString(2, surname);
@@ -187,17 +185,17 @@ public class JDBC {
         } else {
             System.out.println("Błąd! Nie dodano użytkownika!");
         }
-
         return preparedStatement.getResultSet();
 
     }
+
     private static void deleteTask(Connection connection) throws SQLException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("podaj id zadania do skasowania");
         String id = scanner.nextLine();
         String sqlDelete = "DELETE FROM task WHERE id = ?";
         PreparedStatement preparedStatement = connection.prepareStatement(sqlDelete);
-        preparedStatement.setInt(1,Integer.parseInt(id));
+        preparedStatement.setInt(1, Integer.parseInt(id));
         preparedStatement.executeUpdate();
     }
 
@@ -209,7 +207,7 @@ public class JDBC {
         PreparedStatement statement = connection.prepareStatement(sqlSearch);
         statement.setInt(1, Integer.parseInt(id));
         ResultSet resultSetSearch = statement.executeQuery();
-   //     System.out.println("imię, nazwisko, login, hasło, email");
+        //     System.out.println("imię, nazwisko, login, hasło, email");
         return resultSetSearch;
     }
 
@@ -224,6 +222,7 @@ public class JDBC {
         System.out.println("nazwa, opis, wykonano, priorytet, user id");
         return resultSetSearch;
     }
+
     static ResultSet searchAllTasks(Connection connection) throws SQLException {
         String sqlSearch = "SELECT id, name, description FROM task";
         PreparedStatement statement = connection.prepareStatement(sqlSearch);
@@ -236,19 +235,19 @@ public class JDBC {
         String sqlSearch = "SELECT id, name, surname FROM user";
         PreparedStatement statement = connection.prepareStatement(sqlSearch);
         ResultSet resultSetSearch = statement.executeQuery();
-       // System.out.println("nazwa, opis, wykonano, priorytet, user id");
+        // System.out.println("nazwa, opis, wykonano, priorytet, user id");
         return resultSetSearch;
     }
 
 
     public static void showAllColumnsFromResultSet(ResultSet resultSet) throws SQLException {
         ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
-        for (int i = 1; i <= resultSetMetaData.getColumnCount(); i++){
+        for (int i = 1; i <= resultSetMetaData.getColumnCount(); i++) {
             System.out.print(resultSetMetaData.getColumnLabel(i) + ", ");
         }
         System.out.println();
-        while (resultSet.next()){
-            for (int i = 1; i <= resultSetMetaData.getColumnCount(); i++){
+        while (resultSet.next()) {
+            for (int i = 1; i <= resultSetMetaData.getColumnCount(); i++) {
                 System.out.print(resultSet.getString(i) + ", ");
             }
             System.out.println();
@@ -261,39 +260,52 @@ public class JDBC {
         System.out.println("wybierz akcję: 1 - logowanie, 2 - dodaj użytkownika");
         Scanner scannerLogin = new Scanner(System.in);
         String login = scannerLogin.nextLine();
-        switch (login) {
-            case "1":
-                System.out.println("logowanie");
-                loginUser(connection);
-                break;
-            case "2":
-                 createUser(connection);
-                 break;
-            default:
-                System.out.println("wybierz akcję");
-        showAllColumnsFromResultSet(searchAllUsers(connection));
-        System.out.println("wybierz akcję: 1 - wyszukaj zadanie, 2 - wyszukaj użytkownika, 3 - zmień status zadania, 4 - usuń zadanie");
-        Scanner scannerAction = new Scanner(System.in);
-        String action = scannerAction.nextLine();
-        switch (action) {
-            case "1":
-                showAllColumnsFromResultSet(searchTask(connection));
-                break;
-            case "2":
-                showAllColumnsFromResultSet(searchUser(connection));
-                break;
-            case "3":
-                updateStatusOfTask(connection);
-                break;
-            case "4":
-                deleteTask(connection);
-                break;
-            default:
-                System.out.println("wybierz akcję");
+
+        if (login.equals("1") || login.equals("2")) {
+            switch (login) {
+                case "1":
+                    System.out.println("logowanie");
+                    loginUser(connection);
+                    break;
+                case "2":
+                    createUser(connection);
+                    break;
+                default:
+                    System.out.println("wybierz akcję: 1 - logowanie, 2 - dodaj użytkownika");
+            }
+        } else {
+            System.out.println("test");
+        }
+//showAllColumnsFromResultSet(searchAllUsers(connection));
+        do {
+            System.out.println("wybierz akcję: 1 - wyszukaj zadanie, 2 - wyszukaj użytkownika, 3 - zmień status zadania, 4 - usuń zadanie, 5 - zamknij program");
+            Scanner scannerAction = new Scanner(System.in);
+            String action = scannerAction.nextLine();
+            if (action.equals("1") || action.equals("2") || action.equals("3") || action.equals("4") || action.equals("5")) {
+                switch (action) {
+                    case "1":
+                        showAllColumnsFromResultSet(searchTask(connection));
+                        break;
+                    case "2":
+                        showAllColumnsFromResultSet(searchUser(connection));
+                        break;
+                    case "3":
+                        updateStatusOfTask(connection);
+                        break;
+                    case "4":
+                        deleteTask(connection);
+                        break;
+                    case "5":
+                        exit(1);
+                        break;
+                    default:
+                        System.out.println("wybierz akcję: 1 - wyszukaj zadanie, 2 - wyszukaj użytkownika, 3 - zmień status zadania, 4 - usuń zadanie, 5 - zamknij program");
+                }
 
 
-
+            } else {
+                System.out.println("test1");
+            }
+        } while (true);
     }
-}
-}
 }
